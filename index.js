480a0f16a353e7afc1962bbafc9a89d348605747
Mark <157068659+markmcla74@users.noreply.github.com>
2024-06-20 13:56:38 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
- document.getElementById('js-navbar-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('js-menu');
-    menu.classList.toggle('show');
-});
-
-document.getElementById('menu-icon').addEventListener('click', function() {
-    var navList = document.getElementById('nav-list');
-    navList.classList.toggle('active');
-});
